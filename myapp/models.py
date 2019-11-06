e5454c4e3e45dd794badb2f8d82383c7837fd37a
achipiz-158 <hyachipiz@gmail.com>
2019-11-06 17:25:42 -0500

    Modelo API

---
+++ myapp/models.py
@@ -0,0 +1,35 @@
+from django.db import models
+from django.conf import settings
+
+
+class Tenant(models.Model):
+    name = models.CharField(max_length=100)
+
+
+class Customer(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    list_prince = models.FloatField()
+    id_tenant = models.ForeignKey(Tenant, on_delete=models.PROTECT)
+
+
+class InvoiceItem(models.Model):
+    quantity = models.FloatField()
+    unit_value = models.FloatField()
+    item_value = models.FloatField()
+    id_product = models.ForeignKey(Product, on_delete=models.PROTECT)
+    id_tenant = models.ForeignKey(Tenant, on_delete=models.PROTECT)
+
+
+class Invoice(models.Model):
+    doc_date = models.CharField(max_length=100)
+    doc_number = models.CharField(max_length=100)
+    total_tax = models.IntegerField()
+    total_value = models.IntegerField()
+    id_invoice_item = models.ForeignKey(InvoiceItem, on_delete=models.PROTECT)
+    id_tenant = models.ForeignKey(Tenant, on_delete=models.PROTECT)
